4c8454064d09d70525743a81ad4bf6a087da21d4
bunemekyakilika <kyakilika.b@gmail.com>
2017-09-14 07:49:54 +0100

    Changes

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-/*  TODO: For ikea add itemprop,
-    TODO: Ebay - if no attributes found, use outer tags
-
-*/
 var tags = document.querySelectorAll('p,h1,h2,h3,h4,h5,h6,span,a,div,td,strong,li');
 for (var i = 0; i < tags.length; i++) {
-    var tag = tags[i];
-    if(tag.innerText != "" && tag.children.length == 0){
-              // Get parent id/class
-              var currentTag = tag;
-              var path = [];
-              var counter = 0;
-              do{
+  (function () {
+      var tag = tags[i];
+      if(tag.innerText != "" && tag.children.length <= 1){
+                // Get parent id/class
+                var currentTag = tag;
+                var path = [];
+                var counter = 0;
+                do{
+                  var newParentTag = currentTag.parentNode;
+                  // Get oldTag position in parent
+                  var nodes = Array.prototype.slice.call(newParentTag.children );
+                  var pos = nodes.indexOf( currentTag );
+                  path[counter] = pos.toString();
 
-                newParentTag = currentTag.parentElement;
-                // Get oldTag position in parent
-                var nodes = Array.prototype.slice.call(newParentTag.children );
-                var pos = nodes.indexOf( currentTag );
-                path[counter] = pos.toString();
 
-
-                if(newParentTag.className != "" || newParentTag.id != ""){
-                    tag.slot = path.join("/");
-                    path.reverse();
-                    tag.onclick = function(){
-                        var dataArray = [];
-                        dataArray.push({
-                          "class" : this.className,
-                          "id"  : this.id,
-                          "text"  : this.innerText,
-                          "tag" : this.tagName,
-                          "count" : "0",
-                          "parentId" : newParentTag.id,
-                          "parentClass" : newParentTag.className,
-                          "location" : this.slot
-                        });
-                        alert(JSON.stringify(dataArray));
-                        this.classList.add('buneme_fluctuate');
-                    }
-                    break;
-                }
-                currentTag = newParentTag;
-                counter ++;
-              }while(currentTag.parentElement != null);
-      }
-    }
+                  if(newParentTag.className != "" || newParentTag.id != ""){
+                      tag.slot = path.join("/");
+                      path.reverse();
+                      tag.onclick = function(){
+                          var dataArray = [];
+                          dataArray.push({
+                            "direct-parent-id" : tag.parentNode.id,
+                            "direct-parent-class" : tag.parentNode.className,
+                            "direct-text" : tag.innerText,
+                            "clicked" : tag.tagName,
+                            "class" : this.className,
+                            "id"  : this.id,
+                            "text"  : this.innerText,
+                            "tag" : this.tagName,
+                            "count" : "0",
+                            "parentId" : newParentTag.id,
+                            "parentClass" : newParentTag.className,
+                            "location" : this.slot
+                          });
+                          alert(JSON.stringify(dataArray));
+                          this.classList.add('buneme_fluctuate');
+                          event.stopPropagation()
+                      }
+                      break;
+                  }
+                  currentTag = newParentTag;
+                  counter ++;
+                }while(currentTag.parentNode != null);
+        }
+  }()); // immediate invocation
+}
